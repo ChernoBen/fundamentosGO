8d1d856d5b6bbb366e577196010dabaa5bc8cabc
Benjamim <benjamim.francisco@hotmail.com>
2020-07-28 16:52:30 -0300

    composicao de interfaces

---
+++ tiposStruct/composicao/composicao.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+//compor uma nova interface a partir de 2 outras interfaces
+type esportivo interface {
+	ligarTurbo()
+}
+
+type luxuoso interface {
+	fazerbaliza()
+}
+
+//composicao de interfaces
+type esportivoluxuoso interface {
+	luxuoso
+	esportivo
+}
+
+type bmw7 struct {
+}
+
+func (b bmw7) ligarTurbo() {
+	fmt.Println("TurboLigado...")
+}
+
+func (b bmw7) fazerbaliza() {
+	fmt.Println("fazendo baliza...")
+}
+
+func main() {
+	var b esportivoluxuoso = bmw7{}
+	b.ligarTurbo()
+	b.fazerbaliza()
+}
