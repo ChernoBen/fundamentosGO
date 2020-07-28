b340ff4e7cad6e04a9eccec4327dfe23f57355bd
Benjamim <benjamim.francisco@hotmail.com>
2020-07-28 17:12:36 -0300

    interface dinamica

---
+++ tiposStruct/tipoInterface/tipoInterface.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+// definindo uma interface de tipo dinamico
+type dinamico interface{}
+
+type curso struct {
+	nome string
+}
+
+func main() {
+	var coisa dinamico
+	var curso dinamico = curso{"go"}
+	fmt.Println(curso)
+	fmt.Println(coisa)
+	coisa = 3
+	fmt.Println(coisa)
+	coisa = "string"
+	fmt.Println(coisa)
+	coisa = true
+	fmt.Println(coisa)
+
+}
+
+/*
+Go é fortemente tipado mas existem execessoes
+se eu criar uma interface dinamica(sem tipo),
+posso declarar variaveis como se a linguagem não fosse tipada
+*/
